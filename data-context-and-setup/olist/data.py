1673e4239c0c0f07693f46228986faa82a3095fd
Thomas Johnson <johnsonthomas467@gmail.com>
2022-12-19 14:00:49 +0000

    get_data function complete

--- data-context-and-setup/olist/data.py
+++ data-context-and-setup/olist/data.py
@@ -14,7 +14,22 @@ class Olist:
             # Use __file__ instead as an absolute path anchor independant of your usename
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        csv_path = os.path.join(root_dir, "data", "csv")
+
+        file_names = [file for file in os.listdir(csv_path) if file.endswith('.csv')]
+
+        key_names = [key_name
+            .replace('olist_', '')
+            .replace('_dataset', '')
+            .replace('.csv', '')
+            for key_name in file_names]
+
+        data = {}
+        for key, file in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path,file))
+
+        return data
 
     def ping(self):
         """
