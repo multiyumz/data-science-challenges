62efd522fdb412044d2e0e6d15e53ada0b67a8b0
Thomas Johnson <johnsonthomas467@gmail.com>
2023-03-05 15:03:46 +0000

    coded out main_local

--- data-train-at-scale/taxifare/interface/main_local.py
+++ data-train-at-scale/taxifare/interface/main_local.py
@@ -60,10 +60,10 @@ def preprocess_and_train(min_date:str = '2009-01-01', max_date:str = '2015-01-01
     train_length = int(len(data) * (1-split_ratio))
     data_train = data.iloc[:train_length, :].sample(frac=1)
     data_val = data.iloc[train_length:, :].sample(frac=1)
-    X_train = data_train.drop("fair_amout", axis=1)
-    y_train = data_train[['fair_amount']]
-    X_val = data_val.drop("fair_amount", axis=1)
-    y_val = data_val[['fair_amount']]
+    X_train = data_train.drop("fare_amount", axis=1)
+    y_train = data_train[['fare_amount']]
+    X_val = data_val.drop("fare_amount", axis=1)
+    y_val = data_val[['fare_amount']]
 
     # Create (X_train_processed, X_val_processed) using `preprocessor.py`
     # Luckily, our preprocessor is stateless: We can "fit_transform" both X_train and X_val without data-leaks!
@@ -98,6 +98,83 @@ def preprocess_and_train(min_date:str = '2009-01-01', max_date:str = '2015-01-01
     save_model(model=model)
 
     print("✅ preprocess_and_train() done")
+
+
+def preprocess(min_date: str = '2009-01-01', max_date: str = '2015-01-01') -> None:
+    """
+    Query and preprocess the raw dataset iteratively (by chunks).
+    Then store the newly processed (and raw) data on local hard-drive for later re-use.
+
+    - If raw data already exists on local disk:
+        - use `pd.read_csv(..., chunksize=CHUNK_SIZE)`
+
+    - If raw data does not yet exists:
+        - use `bigquery.Client().query().result().to_dataframe_iterable()`
+
+    """
+    print(Fore.MAGENTA + "\n ⭐️ Use case: preprocess by batch" + Style.RESET_ALL)
+
+    from taxifare.ml_logic.data import clean_data
+    from taxifare.ml_logic.preprocessor import preprocess_features
+
+    min_date = parse(min_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+    max_date = parse(max_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+
+    query = f"""
+        SELECT {",".join(COLUMN_NAMES_RAW)}
+        FROM {GCP_PROJECT_WAGON}.{BQ_DATASET}.raw_{DATA_SIZE}
+        WHERE pickup_datetime BETWEEN '{min_date}' AND '{max_date}'
+        ORDER BY pickup_datetime
+        """
+    # Retrieve `query` data as dataframe iterable
+    data_query_cache_path = Path(LOCAL_DATA_PATH).joinpath("raw", f"query_{min_date}_{max_date}_{DATA_SIZE}.csv")
+    data_processed_path = Path(LOCAL_DATA_PATH).joinpath("processed", f"processed_{min_date}_{max_date}_{DATA_SIZE}.csv")
+
+    data_query_cache_exists = data_query_cache_path.is_file()
+    if data_query_cache_exists:
+        print("Get a dataframe iterable from local CSV...")
+        chunks = None
+        chunks = pd.read_csv(data_query_cache_exists, chunksize=CHUNK_SIZE, parse_dates=["pickup_datetime"])
+
+    else:
+        print("Get a dataframe iterable from Querying Big Query server...")
+        chunks = None
+        # 🎯 Hints: `bigquery.Client(...).query(...).result(page_size=...).to_dataframe_iterable()`
+        client = bigquery.Client(project=GCP_PROJECT)
+        query_job = client.query(query)
+        result = query_job.result(page_size=CHUNK_SIZE)
+        chunks = result.to_dataframe_iterable()
+
+    for chunk_id, chunk in enumerate(chunks):
+        print(f"processing chunk {chunk_id}...")
+
+        # Clean chunk
+        chunk_clean = clean_data(chunk)
+
+        # Create chunk_processed
+        # 🎯 Hints: Create (`X_chunk`, `y_chunk`), process only `X_processed_chunk`, then concatenate (X_processed_chunk, y_chunk)
+        X_chunk = chunk_clean.drop("fare_amount", axis=1)
+        y_chunk = chunk_clean[["fare_amount"]]
+        X_processed_chunk = preprocess_features(X_chunk)
+        chunk_processed = pd.DataFrame(np.concatenate((X_processed_chunk, y_chunk), axis=1))
+
+        # Save and append the processed chunk to a local CSV at "data_processed_path"
+        # 🎯 Hints: df.to_csv(mode=...)
+        # 🎯 Hints: We want a CSV without index nor headers (they'd be meaningless)
+        chunk_processed.to_csv(data_processed_path,
+                               mode='w' if chunk_id==0 else "a",
+                               header=False,
+                               index=False)
+
+        # Save and append the raw chunk if not `data_query_cache_exists`
+        if not data_query_cache_exists:
+            chunk.to_csv(data_query_cache_path,
+                         mode="w" if chunk_id==0 else "a",
+                         header=True if chunk_id==0 else False,
+                         index=False)
+
+    print(f"✅ data query saved as {data_query_cache_path}")
+    print("✅ preprocess() done")
 
 
 def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
